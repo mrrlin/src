ea6959126e51259d908aaee7569464cbb07e4fd3
mrrlina <angelochek.kulyaeva@mail.ru>
2020-04-25 09:28:49 +0300

    one

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,20 @@ namespace src
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размер массива: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] Routes = new int[n];
+
+            Console.WriteLine("Введите номер маршрута: ");
+            int NumberBus = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество остановок: ");
+            int QuantityOfStop = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите время в пути (в минутах): ");
+            int TimeTravel = int.Parse(Console.ReadLine());
+
+            Route One = new Route(NumberBus, QuantityOfStop, TimeTravel);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ src/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Route
+    {
+        int RouteNumber;
+        int NumberOfStops;
+        int TravelTime;
+
+        public Route(int route_number, int number_of_stops, int travel_time)
+        {
+            RouteNumber = route_number;
+            NumberOfStops = number_of_stops;
+            TravelTime = travel_time;
+        }
+    }
+}
